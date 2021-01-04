1c77ab5a75cfae18c4ed2f4a2f8609e70d536649
FredrikGehrke <57892335+FredrikGehrke@users.noreply.github.com>
2021-01-04 14:01:21 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <navbar />
+    <firstSection />
+    <secondSection />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navbar from './components/navbar'
+import firstSection from './components/firstSection'
+import secondSection from './components/secondSection'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    navbar,
+    firstSection,
+    secondSection
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: 0;
+
 }
+
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+    <h2>SBUH | Navbar</h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style scoped>
+
+.navbar {
+  background-color: rgba(179, 182, 43, 0.644);
+}
+
+</style>
