149fec8c9b447fffd95c5cb23678396f464fe1df
FredrikGehrke <57892335+FredrikGehrke@users.noreply.github.com>
2021-01-05 10:19:08 +0100

    navbar & theme-color

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,20 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: 0;
+  letter-spacing: 1.5px;
+  font-family: 'Open Sans', sans-serif;
+}
 
+:root {
+  --theme-color: #f38d07;
+}
+
+.theme-bg {
+  background-color: var(--theme-color)
+}
+
+.theme-color {
+  color: var(--theme-color)
 }
 
 </style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="navbar">
-    <h2>SBUH | Navbar</h2>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark white">
+    <div class="container">
+
+      <a class="navbar-brand text-dark" href="#">SBUH</a>
+
+      <button class="navbar-toggler theme-bg" type="button" data-toggle="collapse" data-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Boka</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Kontakt</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-dark" href="#">Foton</a>
+          </li>
+        </ul>
+        <button type="button" class="btn theme-bg text-white">Boka</button>
+        <a id="mail-icon" href="mailto:fredrikg@nomea.se"><i class="fas fa-envelope fa-2x theme-color px-2"></i></a>
+      </div>
+
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -13,8 +38,27 @@ export default {
 
 <style scoped>
 
-.navbar {
-  background-color: rgba(179, 182, 43, 0.644);
+.navbar-brand {
+  font-weight: 800;
+  font-size: 26px;
+  font-family: 'Roboto Slab', serif;
 }
+
+.nav-link {
+  font-weight: 500;
+  font-size: 18px;
+} .nav-link:hover {
+  color: var(--theme-color) !important;
+}
+
+.btn {
+  font-weight: 600;
+  font-size: 16px;
+  padding: 10px 18px 10px 18px;
+  border-radius: 6px;
+  margin-left: 0;
+}
+
+
 
 </style>
