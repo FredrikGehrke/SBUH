44af34e20e4726c213077fe949314fc23f2de80a
FredrikGehrke <57892335+FredrikGehrke@users.noreply.github.com>
2021-01-19 10:53:05 +0100

    content update

--- src/components/kunder.vue
+++ src/components/kunder.vue
@@ -2,7 +2,8 @@
   <div class="mt-5 lighter-bg py-5 ">
     <div class="offset-md-3 col-md-6 col-12 text-center">
       <h4 class="h1 font-weight-bold">Vilka brukar hyra av oss?</h4>
-      <p class="mt-4"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam quos quasi, amet magni ullam eveniet nisi quia molestiae aspernatur consectetur? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, adipisci. Lorem ipsum dolor sit amet. </p>
+      <p class="mt-4">Våra kunder är allt från barnfamiljer till företag. Som vill bygga en egen pool, dränera husgrunden eller vaför inte gräva fibernät. 
+        Det flesta väljer att gräva själva efter en kortare utbildning från oss för att komma ner i kostnader, men hyr inte bara maskiner utan hyr även oss att utföra arbetet.</p>
     </div>
   </div>
 </template>
