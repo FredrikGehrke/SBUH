f915cdb5e859001c56f2213bf794a45f330bd530
DESKTOP-MBP1NT4\anton <eliasson.englund.anton@gmail.com>
2021-01-27 10:33:32 +0100

    Chatt

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="bg-img">
-    <div class="elfsight-app-10f96e3d-63af-4ae4-bab5-4c10d2a875fb"></div>
         <div class="my-overlay">
             <section class="text-center">
                 <div class="col-md-12 p-0">
